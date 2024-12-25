b35863dd837d417cb6d19364158c42bd2f8e0e99
rid <71275822+r7997@users.noreply.github.com>
2024-12-25 19:29:21 +0100

    Update index.py

--- web/index.py
+++ web/index.py
@@ -379,4 +379,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Ensure the database is created upon starting the app
     
-    app.run(host="0.0.0.0", port=8080)
+    app.run()
