c555ca6f0a4362edb5211c1c036ab471c8afbb3e
rid <71275822+r7997@users.noreply.github.com>
2024-12-25 19:26:20 +0100

    Update index.py

--- web/index.py
+++ web/index.py
@@ -375,6 +375,8 @@ def password_recovery():
     return 'Password Recovery Page (not implemented)'
 
 if __name__ == '__main__':
+    
     with app.app_context():
         db.create_all()  # Ensure the database is created upon starting the app
-    app.run(debug=True)
+    
+    app.run(host="0.0.0.0", port=8080)
